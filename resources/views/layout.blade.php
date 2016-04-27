231a26ac419388616147f132280ff871c089f07d
Patrick Morgan <patrick.morgan@live.com.au>
2016-04-27 12:19:41 +1000

    Simple homepage and sass/bootstrap/css setup

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Tech Talk</title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+
+	<div class="container">
+		@yield('content')
+	</div>
+
+</body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+	
+	<h1>Tech Talk</h1>
+
+@stop
