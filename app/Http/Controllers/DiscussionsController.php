8ef077deb88af64ce1a0b403a2909ea795195ca0
Patrick Morgan <patrick.morgan@live.com.au>
2016-07-19 16:13:20 +1000

    route bug

--- app/Http/Controllers/DiscussionsController.php
+++ app/Http/Controllers/DiscussionsController.php
@@ -76,8 +76,6 @@ class DiscussionsController extends Controller
      */
     public function show($id)
     {
-        if (! is_int($id) ) abort(404);
-
         $discussion = Discussion::with('comments')->findOrFail($id);
 
         $comments = getOrderedComments($discussion);
